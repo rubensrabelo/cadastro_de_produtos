eec5f801b713396c24cf79153d7fd9cd30c9b3df
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-12-15 11:36:31 -0300

    Creating user repository

---
+++ product/src/main/java/com/register/product/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.register.product.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.register.product.models.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	Optional<User> findByEmail(String email);
+}
